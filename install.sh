f061beadc08a12eeb3761d2b0fc7bc827f9b3170
Phinx <wanyuehan@qq.com>
2021-05-09 19:00:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,5 @@ wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.10/bee-clef_
 sudo dpkg -i bee-clef_0.4.10_amd64.deb
 sudo dpkg -i bee_0.5.3_amd64.deb
 sudo bee-get-addr > addr.txt
-sudo nano /etc/bee/bee.yaml
 chown -R bee:bee /var/lib/bee
-sudo systemctl start bee
 sudo journalctl -u bee -f
