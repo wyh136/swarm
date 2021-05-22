7aefed93b3a04533421277231f3ca37c6019f48d
Phinx <wanyuehan@qq.com>
2021-05-22 15:52:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install jq -y
 wget https://github.com/ethersphere/bee/releases/download/v0.6.0/bee_0.6.0_amd64.deb
-wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.10/bee-clef_0.4.10_amd64.deb
+wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.12/bee-clef_0.4.12_amd64.deb
 sudo dpkg -i bee-clef_0.4.12_amd64.deb
 sudo dpkg -i bee_0.6.0_amd64.deb
 sudo bee-get-addr > addr.txt
