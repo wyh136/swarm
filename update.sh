e80e5b8f01f4d331d82b229312fd4660bbf3f4c9
Phinx <wanyuehan@qq.com>
2021-06-01 19:14:33 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,9 +4,9 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo systemctl stop bee
 sudo cp -r /var/lib/bee/keys ~/keys_backup
-wget https://github.com/ethersphere/bee/releases/download/v0.6.1/bee_0.6.1_amd64.deb
+wget https://github.com/ethersphere/bee/releases/download/v0.6.2/bee_0.6.2_amd64.deb
 wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.12/bee-clef_0.4.12_amd64.deb
 sudo dpkg -i bee-clef_0.4.12_amd64.deb
-sudo dpkg -i bee_0.6.1_amd64.deb
+sudo dpkg -i bee_0.6.2_amd64.deb
 sudo systemctl start bee
 sudo journalctl -u bee -f
